909b0eb6248b3a96a848b7ec47a58188b45526d2
rjha <rajeev.jha@zalando.de>
2026-03-08 12:19:59 +0100

    added location

--- src/main/kotlin/org/example/bookride/controller/LocationController.kt
+++ src/main/kotlin/org/example/bookride/controller/LocationController.kt
@@ -1,6 +1,7 @@
 package org.example.bookride.controller
 
 import org.example.bookride.dto.DriverLocation
+import org.example.bookride.service.LocationService
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -8,10 +9,13 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/location")
-class LocationController {
+class LocationController(
+    val locationService: LocationService,
+) {
     @PostMapping
     fun create(
         @RequestBody driverLocation: DriverLocation,
     ) {
+        locationService.updateLocation(driverLocation)
     }
 }
